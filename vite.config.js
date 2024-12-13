5f50687fa505e015de20c5bd6289fc11a247516d
mukesh0902 <ku.mukesh8474@gmail.com>
2024-12-13 16:54:49 +0530

    reset1

--- vite.config.js
+++ vite.config.js
@@ -5,8 +5,4 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   base: "/shoppingreact",
   plugins: [react()],
-  build: {
-    outDir: 'dist', // Ensure this matches the deployment directory
-  },
 })
-
